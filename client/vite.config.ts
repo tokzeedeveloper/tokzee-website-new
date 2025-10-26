995a27cb595ef9d30615dde2eab4a3f82f30f15e
tokzeedeveloper <developer@tokzee.live>
2025-10-26 11:53:21 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   base: '/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets')
     }
   }
 })
